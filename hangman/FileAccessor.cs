689c7dace16061c1e13c6cd6cf8c4e789e8605af
miguelramirez1972 <32784537+miguelramirez1972@users.noreply.github.com>
2017-11-20 21:37:51 -0600

    Add files via upload

---
+++ hangman/FileAccessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_Console
+{
+    class FileAccessor
+    {
+        /// <summary>
+        /// Gets or sets the path to the file that is being accessed
+        /// </summary>
+        public string FilePath { get; set; }
+
+
+        /// <summary>
+        /// Creates an instance of a FileAccessor, setting
+        /// the FilePath property to the passed value.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public FileAccessor(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+
+
+        /// <summary>
+        /// Returns a random line from the file.
+        /// </summary>
+        /// <returns>Random line from file as a string.</returns>
+        public string GetRandomLine()
+        {
+            StreamReader textIn = new StreamReader(FilePath);
+            //get random word
+            Random myrandom = new Random();
+            int randNum = myrandom.Next(1, 479781);
+
+            //read through file to get word
+            for (int i = 0; i < randNum; i++)
+            {
+                textIn.ReadLine();
+            }
+
+            return textIn.ReadLine();
+
+        }
+    }
+}
