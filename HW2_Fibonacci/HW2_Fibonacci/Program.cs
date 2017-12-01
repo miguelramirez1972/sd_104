d86664e1995f547050e4d6fe27b226275c0a779f
miguelramirez1972 <32784537+miguelramirez1972@users.noreply.github.com>
2017-12-01 17:02:34 -0600

    Add files via upload

---
+++ HW2_Fibonacci/HW2_Fibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homework #2.  Write a program that prompts the user for which number in the Fibonacci 
+            //sequence that they want to see, and then compute it for them.  Do this by writing a 
+            //loop that will start with 1 and 1 as the previous two numbers, adds them, and then stores 
+            //the result as the previous before the next loop iteration.
+
+            
+            Console.WriteLine("Enter Fibonacci number desired: ");
+           int b;
+           b=Convert.ToInt32(Console.ReadLine());
+            
+            int ans = fibonacci(b);
+            Console.WriteLine(ans);
+            Console.ReadLine();
+            //fib [6] is equal to 8
+           
+
+        }
+        static int fibonacci(int n)
+        {
+            int[] fib = new int[n + 1];
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i=2; i<=n;i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            return fib[n];
+
+        }
+    }
+}
---
+++ HW3_Working_with_StreamReader/HW3_Working_with_StreamReader/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_Working_with_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declarations
+            string strChoice = "";
+
+            //menu loop
+            while (strChoice != "3")
+            {
+                //main menu
+                Console.WriteLine("-------Main Menu--------");
+                Console.WriteLine("1.\tAdd Names to a File");
+                Console.WriteLine("2.\tView Names in a File");
+                Console.WriteLine("3.\tExit");
+                strChoice = Console.ReadLine();
+
+
+                //process choice
+                switch (strChoice)
+                {
+                    case "1":
+                        AddNames();
+                        break;
+                    case "2":
+                        ViewNames();
+                        break;
+                    case "3":
+                        Exit();
+                        break;
+                    default:
+                        Console.WriteLine(strChoice + " is not a valid choice.");
+                        break;
+                }
+            }
+
+        }//end Main()
+
+
+        static void AddNames()
+        {
+            //Instantiate a FileStream that will open a file named "names.txt", 
+            FileStream stream1 = new FileStream("names.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter textFile1 = new StreamWriter(stream1);
+
+            Console.WriteLine("Enter a name or type -99 to quit:");
+            string userInput = Console.ReadLine();
+
+            //input loop
+            while (userInput != "-99")
+            {
+                textFile1.WriteLine(userInput);
+                Console.WriteLine("Enter a name or type -99 to quit:");
+                userInput = Console.ReadLine();
+
+            }
+
+            textFile1.Close();
+
+
+
+        }//end AddNames()
+
+        static void ViewNames()
+        {
+            FileStream stream1 = new FileStream("names.txt", FileMode.Open, FileAccess.Read);
+            StreamReader textFile1 = new StreamReader(stream1);
+
+            while (!textFile1.EndOfStream)
+            {
+                
+                Console.WriteLine("List of names: " + textFile1.ReadLine());
+            }
+
+
+
+
+
+            //instantiate a FileStream Object to open "names.txt" with FileMode.Open and FileAccess.Read
+
+            //instantiate a StreamReader Object and pass to it the FileStream Object you created
+
+            //read file in loop while the reader is not at the EndOfStream and display the lines of text
+
+            //Close the file
+
+
+        }//end ViewNames()
+        static void Exit ()
+        {
+            Console.WriteLine("You have selected to exit");
+        }
+
+    }
+    
+}
